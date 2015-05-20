85dba64bfd03f8d4fc18f8faeac5f681382bb663
Stephen Smalley <sds@tycho.nsa.gov>
2015-05-20 12:33:16 -0400

    tbf selinux: enable genfscon labeling for sysfs and pstore files
    Support per-file labeling of sysfs and pstore files based on
    genfscon policy entries.  This is safe because the sysfs
    and pstore directory tree cannot be manipulated by userspace,
    except to unlink pstore entries.
    This provides an alternative method of assigning per-file labeling
    to sysfs or pstore files without needing to set the labels from
    userspace on each boot.  The advantages of this approach are that
    the labels are assigned as soon as the dentry is first instantiated
    and userspace does not need to walk the sysfs or pstore tree and
    set the labels on each boot.  The limitations of this approach are
    that the labels can only be assigned based on pathname prefix matching.
    You can initially assign labels using this mechanism and then change
    them at runtime via setxattr if allowed to do so by policy.
    
    Change-Id: If5999785fdc1d24d869b23ae35cd302311e94562
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Suggested-by: Dominick Grift <dac.override@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -702,12 +702,18 @@ static int selinux_set_mnt_opts(struct super_block *sb,
 		sbsec->flags |= SE_SBPROC | SE_SBGENFS;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if (!strcmp(sb->s_type->name, "debugfs") ||
 	    !strcmp(sb->s_type->name, "sysfs") ||
 	    !strcmp(sb->s_type->name, "pstore"))
 =======
 	if (strcmp(sb->s_type->name, "debugfs") == 0)
 >>>>>>> e014558... selinux: enable per-file labeling for debugfs files.
+=======
+	if (!strcmp(sb->s_type->name, "debugfs") ||
+	    !strcmp(sb->s_type->name, "sysfs") ||
+	    !strcmp(sb->s_type->name, "pstore"))
+>>>>>>> 58b074c... selinux: enable genfscon labeling for sysfs and pstore files
 		sbsec->flags |= SE_SBGENFS;
 
 	/* Determine the labeling behavior to use for this filesystem type. */
